ba3460bcdabe9acb5b6fd27f47b90b2381d04193
Kjartan Bjørset <kjartanbjorset@gmail.com>
2015-10-17 12:44:37 +0200

    config

--- app/config_template.js
+++ app/config_template.js
@@ -1,9 +1,14 @@
 (function(module) {
 
- 	var randomName = "server"+Math.random();
+ 	//var serverName = "server"+Math.random();
+ 	var serverName = "regn";
 
+ 	var hostIp = "localhost";
+ 	var hostIp = "178.62.98.218";
+ 	var hostIp = "84.48.195.109";
+ 	
     module.exports.getName = function () {
-		return randomName;
+		return serverName;
     };
 
 })(module);
